0d2e008cd321f905ffc2b724ed7c96bc78e77838
Nuno Bettencourt <nmb@isep.ipp.pt>
2016-12-11 16:21:51 +0000

    Minor code formatting.

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -8,27 +8,28 @@ import lapr.project.model.CalculatorExample;
  * @author Nuno Bettencourt <nmb@isep.ipp.pt> on 24/05/16.
  */
 class Main {
-    
+
 	/**
 	 * Logger class.
 	 */
-    private static final Logger LOGGER = Logger.getLogger("MainLog");
+	private static final Logger LOGGER = Logger.getLogger("MainLog");
 
 	/**
 	 * Private constructor to hide implicit public one.
 	 */
-	private Main(){
+	private Main() {
 
 	}
 
 	/**
 	 * Application main method.
+	 * 
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
 		CalculatorExample calculatorExample = new CalculatorExample();
-                int value = calculatorExample.sum(3, 5);
-                LOGGER.log(Level.INFO, String.valueOf(value));
-		
+		int value = calculatorExample.sum(3, 5);
+		LOGGER.log(Level.INFO, String.valueOf(value));
+
 	}
 }
