94350e4f7c5c05e737a0255522b5604c9fbc9ce7
Nuno Bettencourt <nmb@isep.ipp.pt>
2019-12-08 10:48:07 +0000

    Update interface. Add getNearestParks example.

--- src/main/java/lapr/project/assessment/Facade.java
+++ src/main/java/lapr/project/assessment/Facade.java
@@ -1,5 +1,9 @@
 package lapr.project.assessment;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Facade implements Serviceable {
 
     @Override
@@ -35,5 +39,27 @@ public class Facade implements Serviceable {
     @Override
     public int getNumberOfBicyclesAtPark(double v, double v1, String s) {
         throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Distance is returns in metres, rounded to the unit e.g. (281,58 rounds
+     * to 282);
+     *
+     * @param v  Latitude in degrees.
+     * @param v1 Longitude in degrees.
+     * @param s  Filename for output.
+     */
+    @Override
+    public void getNearestParks(double v, double v1, String s) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(s));
+            writer.write("41.152712,-8.609297,494");
+            writer.newLine();
+            writer.write("41.145883,-8.610680,282");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
