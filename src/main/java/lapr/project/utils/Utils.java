57e6fcdfbe3fb3573f991b86b13b1473c41bfc4d
1180842 <1180842@isep.ipp.pt>
2019-12-16 10:53:48 +0000

    <G29-129> Utils test [Implementation]

--- src/main/java/lapr/project/utils/Utils.java
+++ src/main/java/lapr/project/utils/Utils.java
@@ -7,15 +7,33 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Utils {
-    public static List<String[]> parseDataFile(String filePath, String valueSeparator) throws FileNotFoundException {
+    private Utils() {}
+
+    public static List<String[]> parseDataFile(String filePath, String valueSeparator, String lineCommentTag) throws FileNotFoundException {
         List<String[]> result = new ArrayList<>();
-        Scanner scanner = new Scanner(new FileReader(filePath));
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] lineValues = line.split(valueSeparator);
-            result.add(lineValues);
+        try (Scanner scanner = new Scanner(new FileReader(filePath))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (!line.startsWith(lineCommentTag)) {
+                    String[] lineValues = line.split(valueSeparator);
+                    result.add(lineValues);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw e;
         }
 
         return result;
+    }
+
+    public static <T> boolean areArraysEqual(T[] a1, T[] a2) {
+        if (a1.length != a2.length)
+            return false;
+
+        for (int i = 0; i < a1.length; i++) {
+            if (!a1[i].equals(a2[i]))
+                return false;
+        }
+        return true;
     }
 }
---
+++ src/test/java/lapr/project/utils/UtilsTest.java
@@ -0,0 +1,53 @@
+package lapr.project.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilsTest {
+
+    @Test
+    void parseDataFileTest() {
+        try {
+            List<String[]> result = Utils.parseDataFile("UtilsTest.data", ";", "#");
+            String[] line = {"Data1", "asoudhwData2", "Data3"};
+            String[] line2 = {"Data4", "asoudhwData5", "Data6"};
+            String[] line3 = {"Data7", "asoudhwData8", "Data9"};
+            Utils.areArraysEqual(line, result.get(0));
+            Utils.areArraysEqual(line2, result.get(1));
+            Utils.areArraysEqual(line3, result.get(2));
+        } catch (FileNotFoundException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void areArraysEqualTest() {
+        String[] a1 = {"Data1", "asoudhwData2", "Data3"};
+        String[] a2 = {"Data4", "asoudhwData5", "Data6"};
+        assertFalse(Utils.areArraysEqual(a1, a2));
+
+        String[] a3 = {"Data1", "asoudhwData2", "Data3"};
+        String[] a4 = {"Data1", "asoudhwData5", "Data3"};
+        assertFalse(Utils.areArraysEqual(a3, a4));
+        assertFalse(Utils.areArraysEqual(a4, a3));
+
+        String[] a5 = {"Data1", "asoudhwData2", "Data3"};
+        String[] a6 = {"Data1", "asoudhwData2", "Daa3"};
+        assertFalse(Utils.areArraysEqual(a5, a6));
+
+        String[] a7 = {"Data1", "asoudhwData2", "Data3", "asd"};
+        String[] a8 = {"Data1", "asoudhwData2", "Data3"};
+        assertFalse(Utils.areArraysEqual(a7, a8));
+
+        assertFalse(Utils.areArraysEqual(a8, a7));
+
+        String[] a9 = {"Data1", "asoudhwData2", "Data3"};
+        String[] a10 = {"Data1", "asoudhwData2", "Data3"};
+        assertTrue(Utils.areArraysEqual(a9, a10));
+    }
+}
