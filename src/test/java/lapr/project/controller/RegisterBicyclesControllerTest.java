3a101d2e915038a6cabdbfe6b69acfeb5a77cb49
Arthur Silva <1180842@isep.ipp.pt>
2019-12-21 17:50:56 +0000

    <G29-249> RegisterBicyclesController tests - trying to fix: jenkins not resolving subfolder file paths #time 5m #done [implementation]

--- src/test/java/lapr/project/controller/RegisterBicyclesControllerTest.java
+++ src/test/java/lapr/project/controller/RegisterBicyclesControllerTest.java
@@ -91,11 +91,11 @@ public class RegisterBicyclesControllerTest {
             fail();
         }
 
-        testInvalidFileDataExceptionCase("testFiles\\MissingColumnBicyclesFile.txt");
-        testInvalidFileDataExceptionCase("testFiles\\MissingColumnBicyclesFile2.txt");
-        testInvalidFileDataExceptionCase("testFiles\\WrongValueBicyclesFile.txt");
-        testInvalidFileDataExceptionCase("testFiles\\WrongValueBicyclesFile2.txt");
-        testInvalidFileDataExceptionCase("testFiles\\WrongValueBicyclesFile3.txt");
+        testInvalidFileDataExceptionCase("testFiles/MissingColumnBicyclesFile.txt");
+        testInvalidFileDataExceptionCase("testFiles/MissingColumnBicyclesFile2.txt");
+        testInvalidFileDataExceptionCase("testFiles/WrongValueBicyclesFile.txt");
+        testInvalidFileDataExceptionCase("testFiles/WrongValueBicyclesFile2.txt");
+        testInvalidFileDataExceptionCase("testFiles/WrongValueBicyclesFile3.txt");
         // Can't test the SQL Exception case because the database is a mock object, so no methods depending on it will fail
     }
 
