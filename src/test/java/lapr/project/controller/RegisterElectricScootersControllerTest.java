f7aa7c9a6075c3214963f280a51362d958e2a145
Arthur Silva <1180842@isep.ipp.pt>
2019-12-25 19:48:50 +0000

    <G29-135> batch register electric scooters remade - tests #time 30m #done [Implementation]

--- src/test/java/lapr/project/controller/RegisterElectricScootersControllerTest.java
+++ src/test/java/lapr/project/controller/RegisterElectricScootersControllerTest.java
@@ -95,39 +95,58 @@ public class RegisterElectricScootersControllerTest {
         try {
             // Check that all these methods have been called once
 
-            FIX BELOW ACCORDING TO NEW VALUES
+//            FIX BELOW ACCORDING TO NEW VALUES
 //            vehiclesInsert.setString(1, description);
 //            vehiclesInsert.setString(2, VehicleType.ELECTRIC_SCOOTER.getSQLName());
 //            vehiclesInsert.setInt(3, 1);
 //            vehiclesInsert.setInt(4, weight);
 //            vehiclesInsert.setFloat(5, aerodynamicCoefficient);
 //            vehiclesInsert.setFloat(6, frontalArea);
-            verify(mockPS1).setInt(1, 40);
-            verify(mockPS1).setFloat(2, 1.34f);
-            verify(mockPS1).setFloat(3, 10.3f);
-            verify(mockPS1).setString(4, ElectricScooterType.URBAN.getSQLName());
-            verify(mockPS1).setString(5, "Cool scooter");
-            verify(mockPS1).setFloat(6, 1.4f);
-            verify(mockPS1).setInt(7, 55);
-            verify(mockPS1).setInt(8, 10000);
-            verify(mockPS1).setDouble(9, -80.222);
-            verify(mockPS1).setDouble(10, 172.12);
 
-            verify(mockPS1).setInt(1, 50);
-            verify(mockPS1).setFloat(2, 1.44f);
-            verify(mockPS1).setFloat(3, 10.5f);
-            verify(mockPS1).setString(4, ElectricScooterType.OFFROAD.getSQLName());
-            verify(mockPS1).setString(5, "Kakkoii scooter");
-            verify(mockPS1).setFloat(6, 1.5f);
-            verify(mockPS1).setInt(7, 56);
-            verify(mockPS1).setInt(8, 10050);
-            verify(mockPS1).setDouble(9, -20.222);
-            verify(mockPS1).setDouble(10, 122.12);
+            // #all
+            verify(mockPS1, times(2)).setString(2, VehicleType.ELECTRIC_SCOOTER.getSQLName());
+            verify(mockPS1, times(2)).setInt(3, 1);
 
-            verify(mockPS1, times(6)).setInt(anyInt(), anyInt());
-            verify(mockPS1, times(6)).setFloat(anyInt(), anyFloat());
-            verify(mockPS1, times(4)).setString(anyInt(), anyString());
-            verify(mockPS1, times(4)).setDouble(anyInt(), anyDouble());
+            // #1
+            verify(mockPS1).setString(1, "Cool scooter");
+            verify(mockPS1).setInt(4, 40);
+            verify(mockPS1).setFloat(5, 1.34f);
+            verify(mockPS1).setFloat(6, 10.3f);
+            // #2
+            verify(mockPS1).setString(1, "Kakkoii scooter");
+            verify(mockPS1).setInt(4, 50);
+            verify(mockPS1).setFloat(5, 1.44f);
+            verify(mockPS1).setFloat(6, 10.5f);
+
+
+            // #1
+            verify(mockPS3).setString(1, "Cool scooter");
+            verify(mockPS3).setString(2, ElectricScooterType.URBAN.getSQLName());
+            verify(mockPS3).setFloat(3, 1.4f);
+            verify(mockPS3).setInt(4, 55);
+            verify(mockPS3).setInt(5, 10000);
+
+            // #2
+            verify(mockPS3).setString(1, "Kakkoii scooter");
+            verify(mockPS3).setString(2, ElectricScooterType.OFFROAD.getSQLName());
+            verify(mockPS3).setFloat(3, 1.5f);
+            verify(mockPS3).setInt(4, 56);
+            verify(mockPS3).setInt(5, 10050);
+
+
+            // #1
+            verify(mockPS3).setDouble(1, -80.222);
+            verify(mockPS3).setDouble(2, 172.12);
+
+            // #2
+            verify(mockPS3).setDouble(1, -20.222);
+            verify(mockPS3).setDouble(2, 122.12);
+
+
+            // #all
+            verify(mockPS3, times(2)).setString(1, "pls");
+            verify(mockPS3).setString(2, "Cool scooter");
+            verify(mockPS3).setString(2, "Kakkoii scooter");
         } catch (Exception e) {
             fail();
         }
